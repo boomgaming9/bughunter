482d80abc24828924c506d7fcd40d5f8b6e33e09
Jens <jenskrone@hotmail.com>
2026-09-21 10:59:52 +0200

    made int target final

--- src/FindNumber.java
+++ src/FindNumber.java
@@ -2,9 +2,9 @@
 
 void main() {
     int[] numbers = {4, 8, 15, 16, 23, 42};
-    int target = 15;
+    final int TARGET = 15;
 
     for (int number : numbers) {
-        IO.println(number == target ? "Found" : "Not found");
+        IO.println(number == TARGET ? "Found" : "Not found");
         }
 }
